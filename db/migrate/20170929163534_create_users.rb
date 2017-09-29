8029063f02713b2c956e77514eaefa21a9673805
Chris Nyaga <nyagachris75@gmail.com>
2017-09-29 19:38:21 +0300

    Migration to create the users table and columns in the database with fields required for authentication.

---
+++ db/migrate/20170929163534_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :name
+      t.string :email
+      t.string :image
+      t.string :oauth_token
+      t.string :oauth_refresh_token
+      t.datetime :oauth_expires_at
+      t.string :national_id
+      t.string :phone_no
+
+      t.timestamps
+    end
+  end
+end
