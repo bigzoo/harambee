e91d257afc6ec90d1d20c350cd7e1f7d124b693a
Chris Nyaga <nyagachris75@gmail.com>
2017-09-29 22:20:50 +0300

    Application router updated with resources for the profile controller pages.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create, :destroy]
   resources :user_harambees
   resources :dashboard, only: :index
+  resources :profile, only: :index
 end
