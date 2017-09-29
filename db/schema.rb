2b3b60e7692eac9102d3e9dce734c31c60b23895
Chris Nyaga <nyagachris75@gmail.com>
2017-09-30 00:58:17 +0300

    Updated database schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170929205757) do
+ActiveRecord::Schema.define(version: 20170929215514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20170929205757) do
     t.string "transaction_confirmation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "receipt_no"
+    t.string "transaction_date"
     t.index ["harambee_id"], name: "index_transactions_on_harambee_id"
   end
 
