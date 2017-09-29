9df3240038c5bdba666380894d6e2e95b3fb6a8d
Chris Nyaga <nyagachris75@gmail.com>
2017-09-29 23:58:40 +0300

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170929205422) do
+ActiveRecord::Schema.define(version: 20170929205757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170929205422) do
     t.boolean "running", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_user_harambees_on_user_id"
   end
 
