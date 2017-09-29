8ab901729122820609fa68501844fc7a75843bb9
Chris Nyaga <nyagachris75@gmail.com>
2017-09-29 21:00:34 +0300

    Google Omniauth initializer updated to an offline access type to recieve the refresh token from google.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'] #, {scope: ['userinfo.email','userinfo.profile']}
-end
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'], { access_type: 'offline' }
