c4c6ff7657ea3019edb8c76460a5ef7ec9f7fad5
Chris Nyaga <nyagachris75@gmail.com>
2017-09-30 09:50:47 +0300

    Login ruote generated in the application router.

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   post '/contribute/callback', to: 'contribute#callback'
   resources :dashboard, only: :index
   resources :profile, only: :index
+  resources :login, only: :index
 end
